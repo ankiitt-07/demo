8de17210a348bd10fa509a5abb539a9e29180016
MEHULSHARMA26 <sharmaamehul26@gmail.com>
2025-06-05 15:38:32 +0530

    mehul says hi!

---
+++ src/main/java/com/example/demo/MehulController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MehulController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello world from Mehul controller!";
+    }
+
+}
